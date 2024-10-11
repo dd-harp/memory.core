0b87e4e7209fab0811c5502fe3ed531fede3abb5
David Smith <smitdave@gmail.com>
2024-10-11 14:50:10 -0700

    Updated

--- Figure10.R
+++ Figure10.R
@@ -1,15 +1,40 @@
-## -------------------------------------------------------------------------------------------------
-#if(redo == TRUE){
-  aa = seq(5, 5*365, by = 5)
-  pr = sapply(aa, d_parasite_detect, FoIpar=foiP3)
-  PR = data.frame(pr=as.vector(pr))
-#  write.table(PR, "pr.txt") 
-#}
-  
+## ------------------------------------------------------------------------------------------------
+library(ramp.falciparum)
+library(viridisLite)
+library(knitr)
+
+
+## ------------------------------------------------------------------------------------------------
+foiP3 = list(hbar = 1, 
+             agePar = par_type2Age(), 
+             seasonPar = par_sinSeason(), 
+             trendPar = par_flatTrend())
+
+
+## ------------------------------------------------------------------------------------------------
+foiP3 = list(hbar = 5/365, 
+             agePar = par_type2Age(), 
+             seasonPar = par_sinSeason(), 
+             trendPar = par_flatTrend())
+
+
+## ------------------------------------------------------------------------------------------------
+aa = seq(5, 5*365, by = 5)
+
+
+## ----eval=F--------------------------------------------------------------------------------------
+## pr = sapply(aa, d_parasite_detect, FoIpar=foiP3)
+## PR = data.frame(pr=as.vector(pr))
+## write.table(PR, "pr.txt")
+
+
+## ------------------------------------------------------------------------------------------------
 moi = meanMoI(aa, foiP3)
 
-#pr = read.table("pr.txt")$pr
+pr = read.table("pr.txt")$pr
 
+
+## ------------------------------------------------------------------------------------------------
 solve_dAoYda(1, foiP3, Amax=5*365, dt=5) -> hybrid
 tm = hybrid$time
 x = hybrid$x[-1]
@@ -28,10 +53,13 @@ xct = pr*(1-exp(-moi))
 aprx = 1 - exp(-m*FQx)
 #aprx1 = (1-exp(-m))*FQy
 
+
+## ------------------------------------------------------------------------------------------------
 plot(aa, xct, type = "l", ylim = c(0,1),
      xlab = "a - Cohort Age",
      ylab = expression(hat(p)[tau](a)))
 lines(aa, aprx, col = "salmon")
+
 #lines(aa, (aprx + aprx1)/2, col = "darkgreen")
 #lines(aa, aprx1, col = "blue")
 
@@ -41,23 +69,15 @@ lines(aa, aprx, col = "salmon")
 
 
 
-## -------------------------------------------------------------------------------------------------
-foiP3$hbar = 5/365
 
-thou = 0:730
-daoi <- dAoI(thou, 5*365, foiP3)
 
-prZ = FQ(thou, 5*365, foiP3)
+## ----purl Figure 10, eval=F----------------------------------------------------------------------
+## print("Making Figure 10")
+## purl("Figure10.Rmd", "Figure10.R")
 
-scl = max(prZ)/max(daoi)
 
-par(mar = c(5,4,4,4))
-plot(thou, prZ, type = "l", col = "darkorange4", lwd=2,
-     xlab = expression(list(alpha, "Age of Infection")), 
-     ylab = expression(F[Q](alpha)))
-
-lines(thou, daoi*scl, type = "l", col = "darkblue") 
-axis(4, pretty(daoi)*scl, pretty(daoi), col = "darkblue")
-mtext(expression(f[A](alpha)), 4, 3)
-
+## ----source Figure 10, eval=F--------------------------------------------------------------------
+## png("Figure10.png", height=360, width=540)
+## source("Figure10.R")
+## invisible(dev.off(dev.cur()))
 
